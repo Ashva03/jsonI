e3c6a4ecdf9f732c6fcbd78909f4929088a17820
pinalvpatoliya <pinalpatoliya5@gmail.com>
2025-03-25 22:20:55 +0530

    fixed padding issue

--- global/components/ui/tabs.tsx
+++ global/components/ui/tabs.tsx
@@ -13,7 +13,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center rounded-md bg-muted p-4 text-muted-foreground',
+      'inline-flex items-center justify-center rounded-md bg-muted p-[8px] text-muted-foreground',
       className,
     )}
     {...props}
